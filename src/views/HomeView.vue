8e2f304527faee63a1450d8c97f16e0967de5e18
Thomas Elbek <vadholm@hotmail.com>
2026-08-23 08:41:06 +0200

    vcxvcxv

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div class="grid grid-rows-2 gap-4">
-      <div class="grid lg:grid-cols-2 gap-4">
+    <div class="grid lg:grid-cols-2 gap-4">
+      <div>
         <div>
           <div class="font-semibold text-lg  border-b border-gray-300">Statens Naturhistoriske Museum</div>
-        </div>
-        <div>
-          <div class="font-semibold text-lg  border-b border-gray-300">Licenshaver</div>
+          <div>
+            {{ state.formularSkabelon }}
+          </div>
         </div>
       </div>
       <div>
-        <div  class="font-semibold text-lg  border-b border-gray-300">Formular</div>
+        <div class="font-semibold text-lg  border-b border-gray-300">Licenshaver</div>
       </div>
+    </div>
+    <div>
+      <div class="font-semibold text-lg  border-b border-gray-300">Formular</div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import api from '@/apijson/';
+import { type formularSkabelonType } from '@/types/formularSkabelonType.ts';
+import { reactive, onMounted } from 'vue';
+
+const state = reactive({
+  formularSkabelon: {} as formularSkabelonType
+});
+
+onMounted(() => {
+  api.get('formularSkabelon.json').then(res => {
+    state.formularSkabelon = res.data;
+  })
+});
+</script>
