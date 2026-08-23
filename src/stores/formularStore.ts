f5a310e0629175ee3ceaae263a0c923655978b11
Thomas Elbek <vadholm@hotmail.com>
2026-08-23 09:10:33 +0200

    cvcxv

--- src/stores/formularStore.ts
+++ src/stores/formularStore.ts
@@ -1,12 +1,27 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import type { formularSkabelonFeltType, formularSkabelonType } from '@/types/formularSkabelonType'
 
 export const useFormularStore = defineStore('formularStore', () => {
-//   const count = ref(0)
-//   const doubleCount = computed(() => count.value * 2)
-//   function increment() {
-//     count.value++
-//   }
+    const formularSkabelon = ref({} as formularSkabelonType);
+    const formularSkabelonFelter = ref([] as formularSkabelonFeltType[])
 
-//   return { count, doubleCount, increment }
+    function setFormularSkabelon(formularSkabelonData: formularSkabelonType): void {
+        formularSkabelon.value = formularSkabelonData;
+    }
+
+    function setFormularSkabelonFelter(formularSkabelonFelterData: formularSkabelonFeltType[]) {
+        formularSkabelonFelter.value = formularSkabelonFelterData;
+    }
+
+    function getFormularSkabelonFelterForSkabelonId(id: number): formularSkabelonFeltType[] {
+        return formularSkabelonFelter.value.filter((item) => item.formularSkabelonId == id && item.erMinimumsFelt);
+    }
+
+    function getFormularSkabelonFelterForSkabelonIdLicensHaver(id: number, licensHaverId: number): formularSkabelonFeltType[] {
+        return formularSkabelonFelter.value.filter((item) => item.formularSkabelonId == id && item.licenshaverId == licensHaverId);
+    }
+
+    return { formularSkabelon, getFormularSkabelonFelterForSkabelonId, getFormularSkabelonFelterForSkabelonIdLicensHaver, setFormularSkabelon, setFormularSkabelonFelter }
 })
+
