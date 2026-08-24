564f1232c3a973feef3d2270fd538dd5eba42508
Thomas Elbek <vadholm@hotmail.com>
2026-08-24 10:18:39 +0200

    fggg

--- src/components/administrator/formularSkabelonEditor.vue
+++ src/components/administrator/formularSkabelonEditor.vue
@@ -2,7 +2,7 @@
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-x-4 gap-y-4">
         <div>
             <div class="text-gray-700 text-lg font-semibold border-b border-gray-400">Skabelonfelter</div>
-            <div class="flex flex-row flex-wrap gap-2 mt-1.5">
+            <div class="flex flex-row flex-wrap gap-3 mt-1.5">
                 <template v-for="skabelonFelt in formularStore.formularSkabelonFelterForAdministrator">
                     <formular-skabelon-felt :skabelonFelt="skabelonFelt"
                         @fjern-felt="fjernFelt"></formular-skabelon-felt>
@@ -11,7 +11,7 @@
         </div>
         <div>
             <div class="text-gray-700 text-lg font-semibold border-b border-gray-400">Mulige felter</div>
-            <div class="flex flex-row flex-wrap gap-2 mt-2">
+            <div class="flex flex-row flex-wrap gap-3 mt-2">
                 <template v-for="felt in formularStore.felterIkkeISkabelon">
                     <muligt-felt :muligtFelt="felt" @felt-valgt="tilfoejFelt"></muligt-felt>
                 </template>
