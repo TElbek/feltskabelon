e51adc5d215d02583bfce2f2870b3d83f61e341a
Thomas Elbek <vadholm@hotmail.com>
2026-09-01 06:15:38 +0200

    Update border color in formularSkabelonFelter for improved visibility

--- src/components/skabelonfelter/formularSkabelonFelter.vue
+++ src/components/skabelonfelter/formularSkabelonFelter.vue
@@ -4,14 +4,14 @@
             <formular-skabelon-header
                 :formularSkabelonNavnId="Number(route.params.skabelonNavnId)"></formular-skabelon-header>
             <div class="grid grid-cols-1 lg:grid-cols-2 gap-3">
-                <div class="border border-gray-300 rounded p-3">
+                <div class="border border-gray-400 rounded p-3">
                     <div class="text-xl text-snhm mb-2">Valgte felter</div>
                     <formular-skabelon-felter-valgt></formular-skabelon-felter-valgt>
                 </div>
-                <div class="border border-gray-300 rounded p-3">
+                <div class="border border-gray-400 rounded p-3">
                     <formular-skabelon-felter-vaelg></formular-skabelon-felter-vaelg>
                 </div>
-                <div class="border border-gray-300 rounded p-3 lg:col-span-2">
+                <div class="border border-gray-400 rounded p-3 lg:col-span-2">
                     <div class="text-xl text-snhm mb-2">Mærkningsformular</div>
                     <maerknings-formular></maerknings-formular>
                 </div>
