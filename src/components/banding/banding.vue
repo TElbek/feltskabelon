a1c77f5babac5119b99f44caa2930658ac285796
Thomas Elbek <vadholm@hotmail.com>
2026-09-04 09:09:24 +0200

    Refactor banding component layout: adjust spacing and improve structure

--- src/components/banding/banding.vue
+++ src/components/banding/banding.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col gap-4 mt-4">
+    <div class="flex flex-col gap-6 mt-2">
         <div class="text-2xl text-snhm">Ringmærkningsdata</div>
 
         <div>
@@ -25,6 +25,7 @@
 
         <tw-show-lg>
             <div class="text-lg text-snhm">Tegnforklaring</div>
+            <div class="border-b border-snhm mt-2 lg:mt-0"></div>
             <div class="mt-2">
                 <button_legend></button_legend>
             </div>
