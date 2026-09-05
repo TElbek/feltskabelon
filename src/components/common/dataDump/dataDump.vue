83859d620e02eca1fa4d65f5ea4d17e4247fe0e8
Thomas Elbek <vadholm@hotmail.com>
2026-09-05 12:13:35 +0200

    Fix import path for dataDumpBandingField component in dataDump.vue

--- src/components/common/dataDump/dataDump.vue
+++ src/components/common/dataDump/dataDump.vue
@@ -14,7 +14,7 @@
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
-import dataDumpBandingField from './dataDumpbandingField.vue';
+import dataDumpBandingField from './dataDumpBandingField.vue';
 import dataDumpFormTemplate from './dataDumpFormTemplate.vue';
 import dataDumpFormTemplateName from './dataDumpFormTemplateName.vue';
 import dataDumpFormTemplateField from './dataDumpFormTemplateField.vue';
