59d930dbbede7cff59456c9f94549f93dd0398af
Thomas Elbek <thel62@gmail.com>
2026-09-10 13:53:33 +0200

    Revise HomeView.vue to enhance welcome message and provide additional context about the mockup and its repository

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,22 @@
 <template>
-    <div class="text-xl text-snhm">Velkommen til Mockup</div>
+    <div class="w-full lg:w-8/10 mt-5">
+        <div class="text-2xl text-snhm mb-2">Welcome to Form Template Mockup - for the Danish Natural History Museum.</div>
+        <div class="border-b border-snhm"></div>
+        <div class="flex flex-col gap-y-0.5">
+            <span>This is the Form Template Mockup site.</span>
+            <span>Feel free to use it to better understand the logics behind Form Templates.</span>
+            <span> Since the Mockup is using an In-Memory-Database, you really can't mess anything up.</span>
+            <div class="flex gap-x-1">
+                <span>The code for the Mockup is in a public Github repository, and can be found at</span>
+                <a class="font-semibold underline"
+                    href="https://github.com/TElbek/feltskabelon">https://github.com/TElbek/feltskabelon</a>
+            </div>
+            <div class="flex gap-x-1">
+                <span>The Mockup is developed using</span>
+                <span class="text-snhm font-semibold">VueJS, Pinia & TailWind CSS on NodeJS</span>
+            </div>
+            <div class="border-b border-snhm"></div>
+            <div class="font-semibold text-snhm text-lg">Thomas Elbek</div>
+        </div>
+    </div>
 </template>
-
-<script setup lang="ts">
-import {useDataStore} from '@/stores/dataStore'
-const dataStore = useDataStore();
-</script>
