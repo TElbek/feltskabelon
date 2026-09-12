3ea0a08dba4e171bd5291344ae2c22c2140cff15
Thomas Elbek <vadholm@hotmail.com>
2026-09-12 12:25:26 +0200

    Refactor scenario list and scenario field list components: adjust template structure for improved readability and maintainability

--- src/components/scenario/scenarioList.vue
+++ src/components/scenario/scenarioList.vue
@@ -1,16 +1,21 @@
 <template>
-    <route-title></route-title>
-    <tw-grid-cols-generic :count="state.scenarioList.length" :offset="0" :itemsPerRow="4">
-        <div v-for="scenario in state.scenarioList" :key="scenario.id" class="bg-white rounded border border-snhm p-2">
-            <div class="text-lg text-snhm">{{ scenario.name }}</div>
-            <scenario-template-list :scenarioId="scenario.id"></scenario-template-list>
-        </div>        
-    </tw-grid-cols-generic>
+    <div>
+        <route-title></route-title>
+        <div class="mt-3">
+            <tw-grid-cols-generic :count="state.scenarioList.length" :offset="0" :itemsPerRow="4">
+                <div v-for="scenario in state.scenarioList" :key="scenario.id"
+                    class="bg-white rounded border border-snhm p-2">
+                    <div class="text-lg text-snhm">{{ scenario.name }}</div>
+                    <scenario-template-list :scenarioId="scenario.id"></scenario-template-list>
+                </div>
+            </tw-grid-cols-generic>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
 import { useDataStore } from '@/stores/dataStore';
-import {type ringingScenarioType} from '@/types/ringingScenarioType.ts';
+import { type ringingScenarioType } from '@/types/ringingScenarioType.ts';
 import scenarioTemplateList from './scenarioTemplateList.vue';
 import { onMounted, reactive } from 'vue';
 import { useRoute } from 'vue-router';
--- src/components/scenariofield/scenarioFieldList.vue
+++ src/components/scenariofield/scenarioFieldList.vue
@@ -1,25 +1,30 @@
 <template>
-    <route-title></route-title>
-    <tw-grid-cols-generic :itemsPerRow="2" :count="groupedByScenarioName.size">
-        <div v-for="[key, value] in groupedByScenarioName" :key="key" class="rounded border border-snhm p-2">
-            <div class="flex justify-between">
-                <div class="flex gap-x-1 text-lg text-snhm text-nowrap">
-                    <span class="me-1">Fields for</span>
-                    <span class="font-semibold">{{ key }}</span>
-                    <span class="me-1">Scenario</span>
+    <div>
+        <route-title></route-title>
+        <div class="mt-3">
+            <tw-grid-cols-generic :itemsPerRow="2" :count="groupedByScenarioName.size">
+                <div v-for="[key, value] in groupedByScenarioName" :key="key" class="rounded border border-snhm p-2">
+                    <div class="flex justify-between">
+                        <div class="flex gap-x-1 text-lg text-snhm text-nowrap">
+                            <span class="me-1">Fields for</span>
+                            <span class="font-semibold">{{ key }}</span>
+                            <span class="me-1">Scenario</span>
+                        </div>
+                        <router-link :to="`/scenario/${value[0].scenarioId}/fields/edit`">Edit</router-link>
+                    </div>
+                    <div class="mt-1">
+                        <tw-flex>
+                            <template v-for="field in sortFieldNames(value)" :key="field.id">
+                                <span class="border border-gray-400 text-gray-600 px-1 rounded">{{ field.placeholder
+                                    }}</span>
+                            </template>
+                        </tw-flex>
+                    </div>
                 </div>
-                <router-link :to="`/scenario/${value[0].scenarioId}/fields/edit`">Edit</router-link>
-            </div>
-            <div class="mt-1">
-                <tw-flex>
-                    <template v-for="field in sortFieldNames(value)" :key="field.id">
-                        <span class="border border-gray-400 text-gray-600 px-1 rounded">{{ field.placeholder
-                            }}</span>
-                    </template>
-                </tw-flex>
-            </div>
+            </tw-grid-cols-generic>
+
         </div>
-    </tw-grid-cols-generic>
+    </div>
 </template>
 
 <script setup lang="ts">
